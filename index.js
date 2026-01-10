2118275ca997001dc6ea0cad5d8a414cbedb2a32
Danilo111267 <danilo111267@gmail.com>
2026-01-10 01:33:28 +0000

    bot funcionando

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const https = require("https");
-const { parse } = require("node-html-parser");
 
 const RSS_URL = "https://rss.app/feeds/GyPL34tb7WUZpcKV.xml";
-const WEBHOOK = process.env.WEBHOOK; // vamos usar secret
+const WEBHOOK = "https://discord.comhttps://discord.com/api/webhooks/1458253167006449735/sCQAazha2aHfsp_cn7SugrfOwGVtny9EfNa6YrBzlMI2yEex1M6iMpuxaKTOw7tzLNyT/api/webhooks/SEU_WEBHOOK";
 
 let ultimoLink = null;
 
-// buscar RSS
 function fetchRSS(url, callback) {
   https.get(url, res => {
     let data = "";
@@ -15,7 +13,6 @@ function fetchRSS(url, callback) {
   });
 }
 
-// enviar ao Discord
 function enviar(link) {
   const convertido = link.replace(
     "https://www.instagram.com",
@@ -38,14 +35,15 @@ function enviar(link) {
   req.end();
 }
 
-// loop
 function checar() {
   fetchRSS(RSS_URL, xml => {
-    const match = xml.match(/<link>(https:\/\/www\.instagram\.com\/[^<]+)<\/link>/);
+    const matches = [...xml.matchAll(
+      /<link>(https:\/\/www\.instagram\.com\/[^<]+)<\/link>/g
+    )];
 
-    if (!match) return;
+    if (matches.length === 0) return;
 
-    const link = match[1];
+    const link = matches[matches.length - 1][1];
 
     if (link !== ultimoLink) {
       ultimoLink = link;
@@ -55,9 +53,5 @@ function checar() {
   });
 }
 
-// checa a cada 5 minutos
 setInterval(checar, 5 * 60 * 1000);
 checar();
-
-
-
